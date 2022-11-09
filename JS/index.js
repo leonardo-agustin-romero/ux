7763a5d7f28388219fb168a8bd119a0e76d0425d
leonardo agustin romero <leo123leo@live.com.ar>
2022-11-08 23:56:28 -0300

    primer commit

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+const btnswitch = document.getElementById("switch");
+
+btnswitch.addEventListener('click',() => {
+    document.body.classList.toggle('dark');
+    btnswitch.classList.toggle('active');
+
+});
